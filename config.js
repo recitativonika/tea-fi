a3057e4d973bda5b6e10eef60539e23a0a712b5c
recitativonika <fool-body-cheddar@duck.com>
2025-02-04 05:22:24 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  amountToWrap: 0.0001 // Amount WPOL to wrap/convert to tPOL
+  amountToWrap: 0.0001, // Amount WPOL to wrap/convert to tPOL
+  autoRestartDelay: 21600 // Delay time in seconds for auto-restart
 };
